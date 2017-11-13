3f6125e28e1c004dd359ed8ddd8d8400ec31665f
Summer <864409241@qq.com>
2017-11-13 23:43:21 +0800

    update web server

--- webServer/server/mux/mux.go
+++ webServer/server/mux/mux.go
@@ -5,18 +5,35 @@ import (
 	"net/http"
 )
 
+//use a map to record the information of routers
 var mux map[string]func(http.ResponseWriter, *http.Request)
 
+//a struct that implement ServeHTTP function int the interface
 type MyHandler struct{}
 
+//main page handler
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World")
 }
 
+//handler of /name
+func name(w http.ResponseWriter, r *http.Request) {
+	//get the query string
+	myname := r.URL.Query().Get("myname")
+	if myname == "" {
+		io.WriteString(w, "Your name value is empty")
+	} else {
+		io.WriteString(w, "Your name is "+myname)
+	}
+}
+
 func (*MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	//add router information and its handler
 	mux["/"] = hello
-	h, ok := mux[r.URL.String()]
+	mux["/name"] = name
+	//return handler according to the path
+	h, ok := mux[r.URL.Path]
 	if ok {
 		h(w, r)
 		return
